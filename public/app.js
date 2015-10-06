ab40ac6c4c80eff2a0ef01d8a42763d42eeffce6
Harry Ganz <hganz@rsmas.miami.edu>
2015-10-06 13:21:22 -0400

    Add angular app, move to public folder

---
+++ public/app.js
@@ -0,0 +1,7 @@
+angular.module('todoApp', [])
+  .controller('TodoListCtrl', ['$scope', function($scope){
+    $scope.items = [
+      {'name':'Meeting', 'description':'Meeting with city council'},
+      {'name':'Dr. Appointement', 'description':'Appointement with Dr. Gordon'}
+    ];
+  }])
