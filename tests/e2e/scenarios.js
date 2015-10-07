4813e87db1307d03de92b4a017f4c2bbffda5fbc
Harry Ganz <hganz@rsmas.miami.edu>
2015-10-07 18:47:46 -0400

    e2e tests for add/delete todo item

--- tests/e2e/scenarios.js
+++ tests/e2e/scenarios.js
@@ -7,7 +7,7 @@ describe('Todo app', function(){
 
 		var todolist = element.all(by.repeater('item in items'));
 		var names = element.all(by.css('#todo-table table tr td:first-child'));
-		var descriptions = element.all(by.css('#todo-table table tr td:nth-child(2)'))
+		var descriptions = element.all(by.css('#todo-table table tr td:nth-child(2)'));
 
 		it('should display 2 items', function(){
 			expect(todolist.count()).toBe(2);
@@ -22,5 +22,31 @@ describe('Todo app', function(){
 		    expect(descriptions.first().getText()).toMatch(/council/i);
 				expect(descriptions.get(1).getText()).toMatch(/Gordon/i);
 		  });
+  });
+
+  describe('Add/Remove in list view', function(){
+    var name = element(by.id('name'));
+    var description = element(by.id('description'));
+    var submitBtn = element(by.id('add-item'));
+    var names = element.all(by.css('#todo-table table tr td:first-child'));
+		var descriptions = element.all(by.css('#todo-table table tr td:nth-child(2)'));
+    var deleteBtns = element.all(by.css('#todo-table table tr td:nth-child(3) button'))
+
+    it('should add a new item', function(){
+        name.sendKeys('New Item');
+        description.sendKeys('A new item');
+        submitBtn.click();
+
+        expect(names.last().getText()).toBe('New Item');
+        expect(descriptions.last().getText()).toBe('A new item');
+    });
+
+    it('Should remove an item', function(){
+        deleteBtns.first().click();
+
+        expect(names.first().getText()).toMatch(/Appointment/);
+        expect(descriptions.first().getText()).toMatch(/Gordon/);
+      });
+
   });
 });
