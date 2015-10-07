fd784e5b2abce11ffe29c4e5cfc189324b660b36
Harry Ganz <hganz@rsmas.miami.edu>
2015-10-06 22:36:43 -0400

    Add and remove todolist items in TodoListCtrl

--- public/app.js
+++ public/app.js
@@ -1,7 +1,26 @@
 angular.module('todoApp', [])
   .controller('TodoListCtrl', ['$scope', function($scope){
+    $scope.name ='';
+    $scope.description = '';
+
     $scope.items = [
       {'name':'Meeting', 'description':'Meeting with city council'},
       {'name':'Dr. Appointment', 'description':'Appointment with Dr. Gordon'}
     ];
+
+    $scope.addItem = function(){
+      var name = $scope.name;
+      var description = $scope.description;
+
+      if(name && description) {
+          $scope.items.push({'name': name,
+           'description': description});
+          $scope.name = '';
+          $scope.description = '';
+      }
+    };
+
+    $scope.removeItem = function(index){
+        $scope.items.splice(index, 1);
+    };
   }]);
