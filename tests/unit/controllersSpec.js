52362853b7ba3c86032e6b21e5f66afe6b798639
Harry Ganz <hganz@rsmas.miami.edu>
2015-10-06 22:36:03 -0400

    Unit tests for adding/deleting todo items

--- tests/unit/controllersSpec.js
+++ tests/unit/controllersSpec.js
@@ -23,4 +23,38 @@ describe('TodoListCtrl', function(){
 
       expect(scope.items[1].description).toMatch(/Gordon/i);
     });
+
+    it('Should add item with name "NewItem" and description "A new item"', inject(function($controller){
+        var scope = {},
+          ctrl = $controller('TodoListCtrl', {$scope:scope})
+
+        scope.name = 'NewItem';
+        scope.description = 'A new item';
+        scope.addItem();
+
+        expect(scope.items[2].name).toBe('NewItem');
+        expect(scope.items[2].description).toBe('A new item');
+
+        scope.removeItem('NewItem')
+    }));
+
+    it('Should remove item with specified index', inject(function($controller){
+      var scope = {},
+          ctrl = $controller('TodoListCtrl', {$scope: scope});
+
+          scope.name = 'NewItem';
+          scope.description = 'A new item';
+          scope.addItem();
+
+          expect(scope.items.length).toBe(3);
+          expect(scope.items[2].name).toBe('NewItem');
+
+          scope.removeItem(2);
+
+          expect(scope.items.length).toBe(2);
+          expect(scope.items.indexOf(function(element, index, arr){
+            element.name === "NewItem"
+          })).toBe(-1);
+    }));
+
 });
