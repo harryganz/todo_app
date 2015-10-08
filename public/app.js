c8f3380af0db186668932bb445e9738296f6399e
Harry Ganz <hganz@rsmas.miami.edu>
2015-10-07 21:11:54 -0400

    Delete todo items by unique id numbers
    Added unique id numbers to item model
    Added max_id counter to TodoListCtrl controller
    Changed removeItem to accept id numbers instead of index
    Modified unit tests to reflect changes in how items are
    removed

--- public/app.js
+++ public/app.js
@@ -2,10 +2,13 @@ angular.module('todoApp', [])
   .controller('TodoListCtrl', ['$scope', function($scope){
     $scope.name ='';
     $scope.description = '';
+    var max_id = 0;
 
     $scope.items = [
-      {'name':'Meeting', 'description':'Meeting with city council'},
-      {'name':'Dr. Appointment', 'description':'Appointment with Dr. Gordon'}
+      {'name':'Meeting', 'description':'Meeting with city council',
+      'id':++max_id},
+      {'name':'Dr. Appointment', 'description':'Appointment with Dr. Gordon',
+      'id': ++max_id}
     ];
 
     $scope.addItem = function(){
@@ -14,13 +17,19 @@ angular.module('todoApp', [])
 
       if(name && description) {
           $scope.items.push({'name': name,
-           'description': description});
+           'description': description, 'id':++max_id});
           $scope.name = '';
           $scope.description = '';
       }
     };
 
-    $scope.removeItem = function(index){
-        $scope.items.splice(index, 1);
+    $scope.removeItem = function(id){
+      var index = $scope.items.map(function(obj, index){
+        if(obj.id === id){
+          return index
+        }
+      }).filter(isFinite)[0]
+
+      $scope.items.splice(index, 1);
     };
   }]);
--- tests/unit/controllersSpec.js
+++ tests/unit/controllersSpec.js
@@ -10,21 +10,24 @@ describe('TodoListCtrl', function(){
       expect(scope.items.length).toBe(2);
     }));
 
-  it('Should have an item named meeting', inject(function($controller){
+  it('Should have an item named meeting and an id of 1', inject(function($controller){
     var scope = {},
       ctrl = $controller('TodoListCtrl', {$scope:scope});
 
     expect(scope.items[0].name).toBe('Meeting');
+    expect(scope.items[0].id).toBe(1);
   }));
 
-  it('Should have an item with a description containing Gordon', function($controller){
+  it('Should have an item with a description containing Gordon and an id of 2', function($controller){
       var scope = {},
         ctrl = $controller('TodoListCtrl', {$scope:scope})
 
       expect(scope.items[1].description).toMatch(/Gordon/i);
+      expect(scope.items[1].id).toBe(2);
     });
 
-    it('Should add item with name "NewItem" and description "A new item"', inject(function($controller){
+    it('Should add item with name "NewItem", description "A new item", and id 3',
+     inject(function($controller){
         var scope = {},
           ctrl = $controller('TodoListCtrl', {$scope:scope})
 
@@ -34,27 +37,24 @@ describe('TodoListCtrl', function(){
 
         expect(scope.items[2].name).toBe('NewItem');
         expect(scope.items[2].description).toBe('A new item');
+        expect(scope.items[2].id).toBe(3);
 
         scope.removeItem('NewItem')
     }));
 
-    it('Should remove item with specified index', inject(function($controller){
+    it('Should remove item with specified id', inject(function($controller){
       var scope = {},
           ctrl = $controller('TodoListCtrl', {$scope: scope});
 
-          scope.name = 'NewItem';
-          scope.description = 'A new item';
-          scope.addItem();
-
-          expect(scope.items.length).toBe(3);
-          expect(scope.items[2].name).toBe('NewItem');
+          expect(scope.items.length).toBe(2);
+          expect(scope.items[1].name).toMatch(/Appointment/i);
 
           scope.removeItem(2);
 
-          expect(scope.items.length).toBe(2);
-          expect(scope.items.indexOf(function(element, index, arr){
-            element.name === "NewItem"
-          })).toBe(-1);
+          expect(scope.items.length).toBe(1);
+          expect(scope.items.filter(function(item){
+            item.id === 2;
+          }).length).toBe(0);
     }));
 
 });
