50e8508b322fabec4eb3273fb21b5d17e92f26a0
Harry Ganz <hganz@rsmas.miami.edu>
2015-10-12 21:58:52 -0400

    Added get/post methods for items

--- public/app.js
+++ public/app.js
@@ -1,27 +1,36 @@
 angular.module('todoApp', [])
-  .controller('TodoListCtrl', ['$scope', function($scope){
+  .controller('TodoListCtrl', ['$scope', '$http', function($scope, $http){
     $scope.name ='';
     $scope.description = '';
     var max_id = 0;
 
-    $scope.items = [
-      {'name':'Meeting', 'description':'Meeting with city council',
-      'id':++max_id},
-      {'name':'Dr. Appointment', 'description':'Appointment with Dr. Gordon',
-      'id': ++max_id}
-    ];
+    $http.get('/items.json').
+    success(function(data) {
+      $scope.items = data;
+    }).
+    error(function(err){
+      // TODO error handling
+      $scope.items = [];
+    });
 
     $scope.addItem = function(){
       var name = $scope.name;
       var description = $scope.description;
 
+
       if(name && description) {
-          $scope.items.push({'name': name,
-           'description': description, 'id':++max_id});
-          $scope.name = '';
-          $scope.description = '';
-      }
-    };
+        var newItem = {'name': name, 'description': description}
+        $http.post('/items', newItem).
+            success(function(){
+              $scope.name = '';
+              $scope.description = '';
+              $scope.items.push(newItem);
+            }).
+            error(function(err){
+              // TODO error handling
+            });
+        }
+  }
 
     $scope.removeItem = function(id){
       var index = $scope.items.map(function(obj, index){
