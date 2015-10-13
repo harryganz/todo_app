5a0ca972bdc21ce00844334b1649121ecd137437
Harry Ganz <hganz@rsmas.miami.edu>
2015-10-12 21:59:16 -0400

    Test get methods for items

--- tests/unit/controllersSpec.js
+++ tests/unit/controllersSpec.js
@@ -1,60 +1,73 @@
 /* Tests for Angular Controllers */
 describe('TodoListCtrl', function(){
 
+  // Load module before each test
   beforeEach(module('todoApp'));
 
-  it('Should create "items" model with 2 items', inject(function($controller){
-      var scope = {},
-        ctrl = $controller('TodoListCtrl', {$scope:scope});
+  // Declare scope, controller, and mock back-end
+  var scope, ctrl, $httpBackend;
 
-      expect(scope.items.length).toBe(2);
+  describe('Get list of items', function(){
+    // Mock up backend responses before each test
+    beforeEach(inject(function(_$httpBackend_,
+      $rootScope, $controller){
+        // Set up backend
+        $httpBackend = _$httpBackend_;
+        $httpBackend.expectGET('/items.json').
+          respond([
+            {'name':'Meeting', 'description':'Meeting with city council',
+            'id':1},
+            {'name':'Dr. Appointment', 'description':'Appointment with Dr. Gordon',
+            'id': 2}
+          ]);
+
+        // Set up scope and controller
+        scope = $rootScope.$new();
+        ctrl = $controller('TodoListCtrl', {$scope: scope});
     }));
 
-  it('Should have an item named meeting and an id of 1', inject(function($controller){
-    var scope = {},
-      ctrl = $controller('TodoListCtrl', {$scope:scope});
+      it('Should create "items" model with 2 items', function(){
+        expect(scope.items).toBeUndefined();
+        $httpBackend.flush();
 
-    expect(scope.items[0].name).toBe('Meeting');
-    expect(scope.items[0].id).toBe(1);
-  }));
+        expect(scope.items.length).toBe(2);
+      });
 
-  it('Should have an item with a description containing Gordon and an id of 2', function($controller){
-      var scope = {},
-        ctrl = $controller('TodoListCtrl', {$scope:scope})
+    it('Should have an item named meeting and an id of 1', function(){
+      expect(scope.items).toBeUndefined();
+      $httpBackend.flush();
+
+      expect(scope.items[0].name).toBe('Meeting');
+      expect(scope.items[0].id).toBe(1);
+    });
+
+    it('Should have an item with a description containing Gordon and an id of 2', function(){
+      expect(scope.items).toBeUndefined();
+      $httpBackend.flush();
 
       expect(scope.items[1].description).toMatch(/Gordon/i);
       expect(scope.items[1].id).toBe(2);
-    });
+      });
+  });
 
-    it('Should add item with name "NewItem", description "A new item", and id 3',
-     inject(function($controller){
-        var scope = {},
-          ctrl = $controller('TodoListCtrl', {$scope:scope})
-
-        scope.name = 'NewItem';
-        scope.description = 'A new item';
-        scope.addItem();
-
-        expect(scope.items[2].name).toBe('NewItem');
-        expect(scope.items[2].description).toBe('A new item');
-        expect(scope.items[2].id).toBe(3);
-
-        scope.removeItem('NewItem')
-    }));
-
-    it('Should remove item with specified id', inject(function($controller){
-      var scope = {},
-          ctrl = $controller('TodoListCtrl', {$scope: scope});
-
-          expect(scope.items.length).toBe(2);
-          expect(scope.items[1].name).toMatch(/Appointment/i);
-
-          scope.removeItem(2);
-
-          expect(scope.items.length).toBe(1);
-          expect(scope.items.filter(function(item){
-            item.id === 2;
-          }).length).toBe(0);
-    }));
-
+  // describe('Add new item', function(){
+  //   beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
+  //     // Set up backend
+  //     $httpBackend = _$httpBackend_;
+  //     $httpBackend.expectPOST('/item',
+  //       {'name': 'newItem', 'description': 'A new item'}).
+  //       respond(201, '3');
+  //
+  //     scope = $rootScope.$new();
+  //     scope.name = 'newItem';
+  //     scope.description = 'A new item';
+  //     ctrl = $controller('TodoListCtrl', {$scope: scope});
+  //   }));
+  //
+  //   it('Adds an item to items', function(){
+  //     $httpBackend.flush();
+  //
+  //     scope.addItem();
+  //   })
+  // });
 });
