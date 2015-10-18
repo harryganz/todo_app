37c30bea0f68d3fcf6c1d949b00867e6d81da931
Harry Ganz <hganz@rsmas.miami.edu>
2015-10-18 18:52:17 -0400

    Added persistance via postgres

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var negotiate = require('express-negotiate');
 var bodyParser = require('body-parser');
 var json = bodyParser.json();
+var query = require('pg-query');
 
 var app = express();
 
@@ -10,22 +11,19 @@ var app = express();
 // Serve up public directory with angular app
 app.use(express.static('public'));
 
-// Temporary items list
-// TODO add persistance
-var max_id = 0;
-var items = [
-  {'name':'Meeting', 'description':'Meeting with city council',
-  'id': ++max_id},
-  {'name':'Dr. Appointment', 'description':'Appointment with Dr. Gordon',
-  'id': ++max_id}
-];
+// Database connection string
+query.connectionParameters = process.env.DATABASE_URL ||
+  "postgres://harry:Stegastes_variabilis@localhost/todo_development"
 
 // More dynamic routes
 app.
   get('/items.:format?', function(request, response, next){
       request.negotiate(request.params.format, {
         'application/json': function(){
-          response.json(items);
+          query('SELECT * FROM items', function(err, rows){
+            if(err) throw err;
+            response.json(rows);
+          });
         },
         'html': function(){
           response.redirect(301, '/');
@@ -41,26 +39,25 @@ app.
         response.sendStatus(400);
         return false;
       } else {
-        items.push({'name': newItem.name, 'description': newItem.description,
-      'id': ++max_id});
-
-        response.status(201).json(newItem.id);
+        query('INSERT INTO items (name, description) ' +
+        'VALUES ($1, $2)', [newItem.name, newItem.description], function(err){
+          if(err) throw err;
+          response.sendStatus(201)
+        });
       }
     }).
     delete('/items/:id', function(request, response){
       // Get ID
       var id = parseInt(request.params.id);
-      // Find index
-      var index = items.map(function(x){
-        return x.id;
-      }).indexOf(id);
-      // If index exists, delete it
-      if(index > -1){
-        items.splice(index, 1);
-        response.sendStatus(200);
-      } else {
-        response.sendStatus(400);
-      }
+      // Delete from database
+      query('DELETE FROM items WHERE id=$1', [id], function(err, rows, result){
+        if(err) throw err;
+        if(result.rowCount === 1) {
+          response.sendStatus(200)
+        } else {
+          response.sendStatus(400)
+        }
+      });
     });
 
 module.exports = app;
